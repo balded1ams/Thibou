807677b3592d174b7c134c7f7e3496ef07380692
Chlorate <bald3diams@gmail.com>
2024-12-28 17:00:10 +0100

    fixed pathfinding

--- src/components/burgerMenu.tsx
+++ src/components/burgerMenu.tsx
@@ -1,6 +1,7 @@
 import { useThemeContext } from "@/hooks/useTheme";
 import { theme } from "@/utils";
 import { useState } from "react";
+import {opacity} from "html2canvas/dist/types/css/property-descriptors/opacity";
 
 const BurgerMenu = () => {
     const { systemTheme, setTheme } = useThemeContext();
@@ -25,7 +26,10 @@ const BurgerMenu = () => {
                     onClick={() => setIsMenuOpen(false)} // Ferme le menu en cliquant à l'extérieur
                 >
                     <div
-                        className="fixed top-28 flex w-4/5 flex-col gap-2 rounded-3xl border border-light-button border-opacity-20 p-4 shadow-lg backdrop-blur-xl md:w-1/3"
+                        className="fixed top-28 flex w-4/5 flex-col gap-2 rounded-3xl p-4 shadow-lg backdrop-blur-xl md:w-1/3"
+                        style={{
+                            border: `1px solid ${systemTheme.background.secondary}`,
+                        }}
                         onClick={(e) => e.stopPropagation()} // Empêche la fermeture du menu quand on clique à l'intérieur
                     >
                         {/* Boutons du menu */}
@@ -38,7 +42,7 @@ const BurgerMenu = () => {
                                 );
                                 setIsMenuOpen(false);
                             }}
-                            className="block w-full rounded-lg border border-light-button bg-light-primary px-4 py-2 text-lg text-light-button transition-all hover:bg-light-button hover:text-light-textSecondary"
+                            className={`block w-full rounded-lg border border-light-button bg-light-primary px-4 py-2 text-lg text-light-button transition-all hover:bg-light-button hover:text-light-textSecondary`}
                         >
                             Theme
                         </button>
