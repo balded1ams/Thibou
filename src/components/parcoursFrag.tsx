2988418210a663be90b07864cf672cb3069b50cd
arowana755 <silure.38@gmail.com>
2024-12-31 15:00:45 +0100

    Mise à jour de parcoursFrag
    Le corp de la page parcoursFrag est presque terminée. Il ne reste qu'à corriger le bug du prompt qui réapparaît à l'infini et à améliorer le bouton

--- src/components/parcoursFrag.tsx
+++ src/components/parcoursFrag.tsx
@@ -1,8 +1,9 @@
 import {useThemeContext} from "@/hooks/useTheme";
 import { kMaxLength } from "buffer";
 import { color } from "bun";
-import Image from "next/image";
-import { isNumberObject } from "util/types";
+import Image from 'next/image'
+import parcoursCplt from '/public/plan-w-parcours-complet.jpg'
+import parcoursFrag from '/public/plan-w-parcours-frag-1.jpg'
 
 const ParcoursFrag = () => {
     const { systemTheme } = useThemeContext();
@@ -15,56 +16,51 @@ const ParcoursFrag = () => {
     }
 
     return (
-        <main className="ml-5" style={{
+        <div className="ml-5" style={{
             color: systemTheme.text.primary,
             backgroundColor: systemTheme.background.primary,
         }}>
             
-            <nav className="flex">
-                <button>Retour</button>
-                <h1>Choisissez votre type de parcours</h1>
+            <nav className="flex my-10">
+                <button className="px-3 border-2 border-solid border-black rounded" style={{
+                    color: systemTheme.text.primary,
+                    backgroundColor: systemTheme.background.secondary,
+                }}>Retour</button>
+                <h1 className="text-3xl text-center grow mr-10">Choisissez votre type de parcours</h1>
             </nav>
             <section className="flex flex-wrap justify-evenly text-sm">
-                <div className="flex flex-col justify-center items-center w-2/5 h-200" style={{
+                <div className="flex flex-col justify-center items-center w-2/5 h-200 pb-3 border-2 border-hidden rounded-3xl" style={{
                     backgroundColor: systemTheme.background.secondary,
                 }}>
                     <a href="#">
-                        <Image 
-                        loader={({ src, width }) =>
-                            `/plan-w-parcours-complet.jpg/${src}?w=${width}`
-                          }
-                          width={kMaxLength}
-                          height={300}
-                          src="/plan-w-parcours-complet.jpg" 
-                          alt="Parcours non divisé"/>
-                        <h2 className="text-xl">Parcours complet</h2>
+                    <Image className="p-10"
+                        src={parcoursCplt}
+                        alt="Parcours non divisé"
+                    />
+                        <h2 className="text-xl text-center mb-5">Parcours complet</h2>
                     </a>
                     <div className="flex">
-                        <p>Temps estimé : </p>
+                        <p className="mr-1">Temps estimé : </p>
                         <span>30 min</span>
                     </div>
                 </div>
-                <div className="flex flex-col justify-center items-center w-2/5 h-200" style={{
+                <div className="flex flex-col justify-center items-center w-2/5 h-200 pb-3 border-2 border-hidden rounded-3xl" style={{
                     backgroundColor: systemTheme.background.secondary,
                 }}>
                     <a href="#" onClick={onFragClick}>
-                        <Image 
-                        loader={({ src, width }) =>
-                            `/plan-w-parcours-frag-1.jpg/${src}?w=${width}`
-                          }
-                        src="/plan-w-parcours-frag-1.jpg"
-                        width={kMaxLength}
-                        height={300} 
-                        alt="Parcours non divisé"/>
-                        <h2 className="text-xl">Parcours fractionné</h2>
+                    <Image className="p-10"
+                        src={parcoursFrag}
+                        alt="Parcours divisé"
+                    />
+                        <h2 className="text-xl text-center mb-5">Parcours fractionné</h2>
                     </a>
                     <div className="flex">
-                        <p>Temps estimé par partie : </p>
+                        <p className="mr-1">Temps estimé par partie : </p>
                         <span>30 min</span>
                     </div>
                 </div>
             </section>
-        </main>
+        </div>
     );
 };
 
