c50ab5f141d7163248cf17090357f32bb50d050a
arowana755 <silure.38@gmail.com>
2025-01-06 17:16:11 +0100

    Mise à jour des utilitaires.
    Les fonctions et procédures utilitaires d'Oeuvre sont presques terminées (quand il ne faut pas directement manipuler la base de données). Mise à jour d'index.ts pour s'accorder à la modification d'OeuvreInter

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,37 +1,35 @@
-import { Musee } from "@/types";
-import { Oeuvre } from "@/types";
+import { Musee } from '@/types';
+import { OeuvreInter } from "@/types";
 
 export const theme = {
     light: {
-        name: "light",
+        name: 'light',
         background: {
-            primary: "#FAF1E4",
-            secondary: "#FDE6CE",
-            button: "#AB8F79",
+            primary: '#FAF1E4',
+            secondary: '#E5C5AE',
         },
         text: {
-            primary: "#453222",
-            secondary: "#FAF1E4",
-            title: "#453222",
+            primary: '#453222',
+            secondary: '#886f59',
+            title: '#453222',
         },
     },
     dark: {
-        name: "dark",
+        name: 'dark',
         background: {
-            primary: "#011926",
-            secondary: "#000C13",
-            button: "#00E980",
+            primary: '#011926',
+            secondary: '#000C13',
         },
         text: {
-            primary: "#235A68",
-            secondary: "#00E980",
-            title: "#00E980",
+            primary: '#235A68',
+            secondary: '#00E980',
+            title: '#00E980',
         },
     },
 };
 
 export const musee: Musee = {
-    name: "Grand Musée Imaginaire",
+    name: 'Grand Musée Imaginaire',
     map: [
         [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0],
         [0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0],
@@ -52,16 +50,15 @@ export const musee: Musee = {
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    ],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ]
 };
 
-export const oeuvres: Oeuvre[] = [
-    { name: "Statue 1", description: "Une belle statue", coordinates: [0, 3] },
-    { name: "Peinture 1", description: "Un chef-d'œuvre", coordinates: [0, 3] },
-    {
-        name: "Sculpture",
-        description: "Une sculpture imposante",
-        coordinates: [0, 3],
-    },
+
+export const oeuvres: OeuvreInter[] = [
+    { name: 'Statue 1', description: 'Une belle statue', coordinates: [0, 3], adapteMalvoyants: true, type: 'Statue' },
+    { name: 'Peinture 1', description: 'Un chef-d\'œuvre', coordinates: [0, 3], adapteMalvoyants: false, type: 'Peinture' },
+    { name: 'Sculpture', description: 'Une sculpture imposante', coordinates: [0, 3], adapteMalvoyants: true, type: 'Sculpture' }
 ];
+
+
