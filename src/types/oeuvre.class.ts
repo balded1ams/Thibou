329a22e5db7ab823015673a6f976c96823cdff7a
arowana755 <silure.38@gmail.com>
2025-01-06 17:12:13 +0100

    Ajout des setteurs de Oeuvre

--- src/types/oeuvre.class.ts
+++ src/types/oeuvre.class.ts
@@ -3,21 +3,29 @@ import { OeuvreInter } from ".";
 export class OeuvreClass implements OeuvreInter {
     private date: string;
     private adapteMalvoyants: boolean;
-    private name: string;
+    private nomOeuvre: string;
     private description: string;
-    private coordinates: [number, number];
+    private coordonnees: [number, number];
     private type: string;
     private nomAuteur: string;
 
-    constructor(oeuvreInter: OeuvreInter, dateOeuvre: string, auteur: string) {
+    public constructor(oeuvreInter: OeuvreInter, dateOeuvre: string, auteur: string) {
         this.adapteMalvoyants = oeuvreInter.adapteMalvoyants;
-        this.name = oeuvreInter.name;
+        this.nomOeuvre = oeuvreInter.name;
         this.description = oeuvreInter.description;
-        this.coordinates = oeuvreInter.coordinates;
+        this.coordonnees = oeuvreInter.coordinates;
         this.type = oeuvreInter.type;
         this.date = dateOeuvre;
         this.nomAuteur = auteur;
     }
 
     public getDate(): string { return this.date; }
+
+    public setAdapteMalvoyants(adapteMalvoyants: boolean) { this.adapteMalvoyants = adapteMalvoyants; }
+    public setNomOeuvre(nomOeuvre: string) { this.nomOeuvre = nomOeuvre; }
+    public setDescription(description: string) { this.description = description; }
+    public setCoordonnees(coordonnees: number[]) { this.coordonnees[0] = coordonnees[0]; this.coordonnees[1] = coordonnees[1]; }
+    public setType(type: string) { this.type = type; }
+    public setDate(date: string) { this.date = date; }
+    public setAuteur(nomAuteur: string) { this.nomAuteur = nomAuteur; }
 }
