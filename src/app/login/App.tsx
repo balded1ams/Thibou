95a369e77bb31518ed2212445302024e3ee4928a
Paul <paaul.rbn@gmail.com>
2025-01-06 17:00:58 +0100

    couleurs

--- src/app/login/App.tsx
+++ src/app/login/App.tsx
@@ -1,17 +1,25 @@
-import AuthThibou from "../../components/auth.thibou";
+import AuthThibou from "../../components/singin";
 import Footer from "@/components/ui/footer";
 import Header from "@/components/ui/header";
+import { useThemeContext } from "@/hooks/useTheme";
 
 
 function App() {
+    const { systemTheme } = useThemeContext();
+
     return (
-        <>
-        <main className={"mx-auto flex h-full max-w-5xl flex-col gap-4 px-4 xl:px-0"}>
-            <Header />
-            <AuthThibou />
-        </main>
-        <Footer />
-        </>
+        <div style={{
+            backgroundColor: systemTheme.background.primary,
+        }}>
+            <main className={"max-w-5xl mx-auto flex h-full flex-col gap-4 px-4"}
+            style={{
+                backgroundColor: systemTheme.background.primary,
+            }}>
+                <Header />
+                <AuthThibou />
+            </main>
+            <Footer />
+        </div>
 );
 }
 
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -6,15 +6,18 @@ import BurgerMenu from "@/components/ui/burgerMenu";
 import { useRouter } from "next/navigation"; // Hook pour la navigation
 
 interface HeaderProps {
-  showAuthButtons?: boolean; // Paramètre optionnel pour afficher les boutons
+    showAuthButtons?: boolean; // Paramètre optionnel pour afficher les boutons
 }
 
 const Header: React.FC<HeaderProps> = ({ showAuthButtons = false }) => {
-  const { systemTheme, setTheme } = useThemeContext();
-  const router = useRouter(); // Initialiser le hook de navigation
+    const { systemTheme, setTheme } = useThemeContext();
+    const router = useRouter(); // Initialiser le hook de navigation
 
     return (
-        <header className="relative flex items-center justify-between py-8">
+        <header
+            className="relative flex items-center justify-between py-8 "
+            style={{ backgroundColor: systemTheme.background.primary }}
+        >
             {/* Logo et titre */}
             <div
                 className="flex cursor-pointer items-center gap-3 hover:underline"
@@ -24,23 +27,21 @@ const Header: React.FC<HeaderProps> = ({ showAuthButtons = false }) => {
                 onClick={() => router.push("/")}
             >
                 <img
-                    className={"max-w-16"}
+                    className="h-12 w-12"
                     src={"/thibou.png"}
                     alt={"Thibou logo"}
                 />
-                <h1
-                    className={`text-2xl font-bold hover:underline lg:text-3xl`}
-                >
-
+                <h1 className="text-2xl font-bold hover:underline lg:text-3xl">
                     Thibou.
                 </h1>
             </div>
 
+            {/* Menu burger pour mobile */}
             <BurgerMenu />
 
             {/* Menu desktop (visible sur grand écran) */}
             <div className="hidden items-center gap-4 lg:flex">
-                {/* Boutons du menu */}
+                {/* Bouton pour changer de thème */}
                 <button
                     onClick={() =>
                         setTheme(
@@ -49,33 +50,44 @@ const Header: React.FC<HeaderProps> = ({ showAuthButtons = false }) => {
                                 : theme.light
                         )
                     }
-                    className="rounded-lg px-4 py-1 text-lg transition-all hover:bg-light-secondary"
+                    className="rounded-lg px-4 py-1 text-lg transition-all hover:opacity-80"
+                    style={{
+                        backgroundColor: systemTheme.background.secondary,
+                        color: systemTheme.text.primary,
+                    }}
                 >
                     Theme
                 </button>
 
-        {/* Boutons "Connexion" et "Inscription" */}
-        {showAuthButtons && (
-          <>
-            <h1
-              className="cursor-pointer rounded-lg px-4 py-1 text-lg transition-all hover:bg-light-secondary"
-              onClick={() => router.push("/login")}
-            >
-              Connexion
-            </h1>
+                {/* Boutons "Connexion" et "Inscription" */}
+                {showAuthButtons && (
+                    <>
+                        <h1
+                            className="cursor-pointer rounded-lg px-4 py-1 text-lg transition-all hover:opacity-80"
+                            onClick={() => router.push("/login")}
+                            style={{
+                                color: systemTheme.text.primary,
+                                backgroundColor: systemTheme.background.secondary,
+                            }}
+                        >
+                            Connexion
+                        </h1>
 
-            <h1
-              className="cursor-pointer rounded-lg bg-light-button px-4 py-1 text-lg text-light-textSecondary
-                                transition hover:bg-light-buttonHover"
-              onClick={() => router.push("/signup")}
-            >
-              Inscription
-            </h1>
-          </>
-        )}
-      </div>
-    </header>
-  );
+                        <h1
+                            className="cursor-pointer rounded-lg px-4 py-1 text-lg transition hover:opacity-80"
+                            onClick={() => router.push("/signup")}
+                            style={{
+                                color: systemTheme.text.secondary,
+                                backgroundColor: systemTheme.background.button,
+                            }}
+                        >
+                            Inscription
+                        </h1>
+                    </>
+                )}
+            </div>
+        </header>
+    );
 };
 
 export default Header;
