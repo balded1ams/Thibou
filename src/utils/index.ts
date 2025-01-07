8879776fbaa75cb941cba5ab99bd14a634c934bf
MOULA Brayan <moulab@hostname.iut2.upmf-grenoble.fr>
2025-01-07 11:16:00 +0100

    dhsoihgoidfjhodfjhodjhfdfgh

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -54,14 +54,25 @@ export const musee: Musee = {
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     ],
+    rooms: [
+        { name: "Salle 1", cooUpLeft: [0, 0], cooDownRight: [4, 9] },
+        { name: "Salle 2", cooUpLeft: [0, 10], cooDownRight: [4, 19] },
+        { name: "Salle 3", cooUpLeft: [5, 0], cooDownRight: [9, 9] },
+        { name: "Salle 4", cooUpLeft: [5, 10], cooDownRight: [9, 19] },
+        { name: "Salle 5", cooUpLeft: [10, 0], cooDownRight: [14, 9] },
+        { name: "Salle 6", cooUpLeft: [10, 10], cooDownRight: [14, 19] },
+        { name: "Salle 7", cooUpLeft: [15, 0], cooDownRight: [19, 9] },
+        { name: "Salle 8", cooUpLeft: [15, 10], cooDownRight: [19, 19] },
+    ],
 };
 
 export const oeuvres: Oeuvre[] = [
-    { name: "Statue 1", description: "Une belle statue", coordinates: [0, 3] },
-    { name: "Peinture 1", description: "Un chef-d'œuvre", coordinates: [0, 3] },
+    { name: "Statue 1", description: "Une belle statue", coordinate: [0, 3] },
+    { name: "Peinture 1", description: "Un chef-d'œuvre", coordinate: [0, 3] },
     {
         name: "Sculpture",
         description: "Une sculpture imposante",
-        coordinates: [0, 3],
+        coordinate: [0, 3],
     },
 ];
+
