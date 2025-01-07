fccf04ab74c1a56c89ca4aa9332eea78924e48c8
Paul <paaul.rbn@gmail.com>
2025-01-07 16:29:01 +0100

    fix tout

--- src/app/signin/App.tsx
+++ src/app/signin/App.tsx
@@ -1,5 +1,4 @@
-import AuthThibou from "../../components/login";
-import { useSearchParams, useRouter } from "next/navigation";
+import Signin from "@/components/signin";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import { useThemeContext } from "@/hooks/useTheme";
@@ -7,30 +6,21 @@ import { useThemeContext } from "@/hooks/useTheme";
 
 function App() {
     const { systemTheme } = useThemeContext();
-    const searchParams = useSearchParams(); // Accéder aux paramètres GET
-    const router = useRouter(); // Router pour changer le paramètre
 
-    // Récupérer la valeur du paramètre "view" (ex: ?view=signup)
-    const view = searchParams.get("view") || "login";
-
-    // Fonction pour basculer entre les vues
-    const handleSwitchView = (newView: "login" | "signup") => {
-        router.push(`?view=${newView}`); // Met à jour l'URL avec le paramètre GET
-    };
     return (
         <div style={{
             backgroundColor: systemTheme.background.primary,
         }}>
             <main className={"max-w-5xl mx-auto flex h-full flex-col gap-4 px-4"}
-            style={{
-                backgroundColor: systemTheme.background.primary,
-            }}>
+                  style={{
+                      backgroundColor: systemTheme.background.primary,
+                  }}>
                 <Header />
-                <AuthThibou />
+                <Signin />
             </main>
             <Footer />
         </div>
-);
+    );
 }
 
 export default App;
