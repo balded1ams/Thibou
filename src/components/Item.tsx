10c4198226fe0cbde1e572cbc6b84ef6948baec3
Paul <paaul.rbn@gmail.com>
2025-01-08 17:25:12 +0100

    CustomCheckBox in items

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useThemeContext } from "@/hooks/useTheme";
+import CustomCheckBox from "@/components/customCheckBox";
 
 interface ItemProps {
     name: string;
@@ -7,7 +8,6 @@ interface ItemProps {
 
 const Item: React.FC<ItemProps> = ({ name }) => {
     const { systemTheme } = useThemeContext();
-    const [selected, setSelected] = useState<"yes" | "no" | "neutral">("neutral");
 
     return (
 
@@ -27,47 +27,7 @@ const Item: React.FC<ItemProps> = ({ name }) => {
             >
                 {name}
             </span>
-            <div className="flex gap-2">
-                <button
-                    className="px-2 py-1 rounded transition"
-                    style={{
-                        backgroundColor:
-                            selected === "no"
-                                ? systemTheme.background.primary
-                                : systemTheme.background.secondary,
-                        color: systemTheme.text.primary,
-                    }}
-                    onClick={() => setSelected("no")}
-                >
-                    ✗
-                </button>
-                <button
-                    className="px-2 py-1 rounded transition"
-                    style={{
-                        backgroundColor:
-                            selected === "neutral"
-                                ? systemTheme.background.primary
-                                : systemTheme.background.secondary,
-                        color: systemTheme.text.primary,
-                    }}
-                    onClick={() => setSelected("neutral")}
-                >
-                    –
-                </button>
-                <button
-                    className="px-2 py-1 rounded transition"
-                    style={{
-                        backgroundColor:
-                            selected === "yes"
-                                ? systemTheme.background.primary
-                                : systemTheme.background.secondary,
-                        color: systemTheme.text.primary,
-                    }}
-                    onClick={() => setSelected("yes")}
-                >
-                    ✓
-                </button>
-            </div>
+            <CustomCheckBox/>
         </div>
     );
 };
