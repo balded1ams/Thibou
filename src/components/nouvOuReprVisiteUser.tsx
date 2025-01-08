3637f984919d173f301b1f65d6f7ea65961d64af
arowana755 <silure.38@gmail.com>
2025-01-08 09:36:39 +0100

    Terminaison du design de la page.

--- src/components/nouvOuReprVisiteUser.tsx
+++ src/components/nouvOuReprVisiteUser.tsx
@@ -1,27 +1,16 @@
 import { useThemeContext } from "@/hooks/useTheme";
+import Image from "next/image";
 
 export const VisiteUtilisateurSup = () => {
     const { systemTheme } = useThemeContext();
 
     return (
         <header>
-            <section className="hidden flex flex-col gap-10 my-40">
-                <nav className="flex">
-                    <button     // Bouton retour
-                        className="rounded-xl px-4 py-1 text-lg"
-                        onClick={() => {}}
-                        style={{
-                            color: systemTheme.text.secondary,
-                            backgroundColor: systemTheme.background.button,
-                        }}>
-                        Retour
-                    </button>
-                    <h1 className="text-3xl grow text-center mr-20">Bonjour "Jean Bon" !</h1>
-                </nav>
-
+            <section className="hidden flex flex-col gap-10 my-20">
+                <h1 className="text-3xl grow text-center">Bonjour "Jean Bon" !</h1>
                 <button 
                     className="self-center text-xl rounded-xl px-6 py-2"
-                    onClick={() => {}}
+                    onClick={() => {/* Afficher la page de choix du type de parcours */}}
                     style={{
                         color: systemTheme.text.secondary,
                         backgroundColor: systemTheme.background.button,
@@ -30,20 +19,11 @@ export const VisiteUtilisateurSup = () => {
                 </button>
             </section>
 
-            <section className="flex flex-col">
-            <button     // Bouton retour
-                        className="self-start rounded-xl px-4 py-1 text-lg mt-5"
-                        onClick={() => {}}
-                        style={{
-                            color: systemTheme.text.secondary,
-                            backgroundColor: systemTheme.background.button,
-                        }}>
-                        Retour
-                    </button>
-                    <h1 className="text-3xl grow text-center my-5">Bonjour "Jean Bon" !</h1>
+            <section className="flex flex-col gap-5 mt-10">
+                    <h1 className="text-3xl grow text-center">Bonjour "Jean Bon" !</h1>
                     <button 
-                    className="text-xl rounded-xl px-6 py-2"
-                    onClick={() => {}}
+                    className="text-2xl rounded-xl px-6 py-2"
+                    onClick={() => {/* Afficher la page de choix du type de parcours */}}
                     style={{
                         color: systemTheme.text.secondary,
                         backgroundColor: systemTheme.background.button,
@@ -61,19 +41,12 @@ export const VisiteUtilisateurInf = () => {
     return (
         <section>
             <div className="hidden">
-                <h1 
-                    className="text-center text-3xl mb-10"
-                    style={{ color: systemTheme.text.title, 
-
-                    }}>
+                <h1 className="text-center text-3xl mb-10">
                     Ou reprendre une visite :
                 </h1>
                 <table 
                     className="flex flex-col rounded-xl"
-                    style={{
-                        backgroundColor: systemTheme.background.button,
-                        color: systemTheme.text.secondary
-                    }}>
+                    style={{ backgroundColor: systemTheme.background.secondary, }}>
                     <thead>
                         <tr className="grid grid-cols-4">
                             <th>Oeuvre en cours :</th>
@@ -83,14 +56,14 @@ export const VisiteUtilisateurInf = () => {
                         </tr>
                     </thead>
                     <tbody className="grid grid-cols-4 grid-rows-">
-                        <tr>
+                        <tr onClick={() => {/* Afficher la suite du parcours sauvegardé */}}>
                             <td></td> {/*Ajouter les éléments en fonction des parcours précédents de l'utilisateur*/}
                             <td></td>
                             <td></td>
                             <td></td>
                         </tr>
 
-                        <tr>
+                        <tr onClick={() => {/* Afficher la suite du parcours sauvegardé */}}>
                             <td></td>
                             <td></td>
                             <td></td>
@@ -99,37 +72,56 @@ export const VisiteUtilisateurInf = () => {
                     </tbody>
                 </table>
             </div>
-            <div>
+            <div className="flex flex-col gap-5">
                 <h1 
-                    className="text-center text-3xl mb-10"
-                    style={{ 
-                        color: systemTheme.text.title, 
-                    }}>
+                    className="text-center text-3xl mb-5 mt-20"
+                    style={{ color: systemTheme.text.title, }}>
                     Ou reprendre une visite :
                 </h1>
 
 
                 <article 
-                    className="flex rounded-xl"
-                    style={{
-                        background: systemTheme.background.button,
-                        color: systemTheme.text.secondary,
-                }}>
-                    <section>
-                        <h2>Oeuvre en cours :</h2>
-                        <span>Text node</span>
+                    className="flex flex-col rounded-xl gap-2 p-1"
+                    onClick={() => {/* Afficher la suite du parcours sauvegardé */}}
+                    style={{ backgroundColor: systemTheme.background.secondary, }}>
+
+                    <section className="flex flex-col">
+                        <h2 className="mr-1">Oeuvre en cours :</h2>
+                        <div className="flex gap-2">
+                            <Image 
+                                className="rounded-xl"
+                                src={""}                    /* Insérer les données de l'image de l'oeuvre */
+                                alt={"Image de l'oeuvre"}
+                                width={200}
+                                height={200}
+                            />
+                            <div className="flex flex-col">
+                                <span 
+                                    className="font-bold"
+                                >
+                                    {/* Insérer ici le nom de l'oeuvre courante */}
+                                </span>
+                                <legend className="italic text-sm">Auteur</legend>
+                            </div>
+                        </div>
                     </section>
-                    <section>
-                        <h2>Salle :</h2>
-                        <span>Text node</span>
+                    <section className="flex">
+                        <h2 className="mr-1">Salle :</h2>
+                        <span className="font-bold">
+                            {/* Insérer ici la salle courante */}
+                        </span>
                     </section>
-                    <section>
-                        <h2>Date de la visite :</h2>
-                        <span>Text node</span>
+                    <section className="flex">
+                        <h2 className="mr-1">Date de la visite :</h2>
+                        <span className="font-bold">
+                            {/* Insérer ici la date */}
+                        </span>
                     </section>
-                    <section>
-                        <h2>Temps restant :</h2>
-                        <span>Text node</span>
+                    <section className="flex">
+                        <h2 className="mr-1">Temps restant :</h2>
+                        <span className="font-bold">
+                            {/* Insérer ici le temps restant */}
+                        </span>
                     </section>
                 </article>
             </div>
