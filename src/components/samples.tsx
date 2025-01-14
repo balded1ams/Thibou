bc2f2d108abbf3022eaae6a52fca5504e2a75e0b
arowana755 <silure.38@gmail.com>
2025-01-14 16:59:06 +0100

    Updating samples
    We now just need to make the reals images, importing those and replacing  the temporary ones and the samples will be finished

--- src/components/samples.tsx
+++ src/components/samples.tsx
@@ -1,16 +1,41 @@
 import { useThemeContext } from "@/hooks/useTheme";
+import Image from "next/image";
+import map from '/public/map.jpg';  // A remplacer par les 3 images voulues
 
 const Samples = () => {
     const { systemTheme } = useThemeContext();
 
     return (
         <div
-            className="flex items-center py-8 "
+            className="flex flex-col items-center py-8 "
             style={{
                 color: systemTheme.text.primary,
             }}
         >
-            <h1>Some samples: </h1>
+            <p>Some samples: </p>
+            <section className="flex flex-wrap gap-4">
+                <Image
+                    src={map}   // A remplacer par l'image voulue
+                    alt={`exemple 1`}
+                    width={300}
+                    height={300}
+                    className="image"
+                />
+                <Image
+                    src={map}   // A remplacer par l'image voulue
+                    alt={`exemple 2`}
+                    width={300}
+                    height={300}
+                    className="image"
+                />
+                <Image
+                    src={map}   // A remplacer par l'image voulue
+                    alt={`exemple 3`}
+                    width={300}
+                    height={300}
+                    className="image"
+                />
+            </section>
         </div>
     );
 };
