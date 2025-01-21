e41a6b9cd84d2bd037fad615976e5072e13bd061
Chlorate <bald3diams@gmail.com>
2025-01-21 15:47:59 +0100

    preference finis

--- src/app/api/preferences/route.ts
+++ src/app/api/preferences/route.ts
@@ -69,6 +69,86 @@ export async function POST(req: Request) {
                 }, {});
                 return NextResponse.json(formattedResult);
             }
+            if((Object.keys(typeOeuvre).filter(key => typeOeuvre[key] === 2).length > 0) ||
+                (Object.keys(Auteur).filter(key => Auteur[key] === 2).length > 0) ||
+                (Object.keys(Mouvement).filter(key => Mouvement[key] === 2).length > 0)||
+                (Object.keys(typeOeuvre).filter(key => typeOeuvre[key] === 1).length > 0) ||
+                (Object.keys(Auteur).filter(key => Auteur[key] === 1).length > 0) ||
+                (Object.keys(Mouvement).filter(key => Mouvement[key] === 1).length > 0)){
+
+                const selectionsAccept = [
+                    { nom: "type_oeuvre", valeurs: Object.keys(typeOeuvre).filter(key => typeOeuvre[key] === 2) },
+                    { nom: "artiste", valeurs: Object.keys(Auteur).filter(key => Auteur[key] === 2) },
+                    { nom: "mouvement", valeurs: Object.keys(Mouvement).filter(key => Mouvement[key] === 2) }
+                ];
+
+                const selectionsRef = [
+                    { nom: "type_oeuvre", valeurs: Object.keys(typeOeuvre).filter(key => typeOeuvre[key] === 1) },
+                    { nom: "artiste", valeurs: Object.keys(Auteur).filter(key => Auteur[key] === 1) },
+                    { nom: "mouvement", valeurs: Object.keys(Mouvement).filter(key => Mouvement[key] === 1) }
+                ];
+
+                const conditionsAcceptTypeOeuvre = selectionsAccept[0].valeurs.map(valeur =>
+                    sql`${oeuvres_musee.type_oeuvre} = ${valeur.toLowerCase()}`
+                );
+
+                const conditionsAcceptArtiste = selectionsAccept[1].valeurs.map(valeur =>
+                    sql`${oeuvres_musee.artiste} = ${valeur.toLowerCase()}`
+                );
+
+                const conditionsAcceptMouvement = selectionsAccept[2].valeurs.map(valeur =>
+                    sql`${oeuvres_musee.mouvement} = ${valeur.toLowerCase()}`
+                );
+
+                const conditionsRefTypeOeuvre = selectionsRef[0].valeurs.map(valeur =>
+                    sql`${oeuvres_musee.type_oeuvre} = ${valeur.toLowerCase()}`
+                );
+
+                const conditionsRefArtiste = selectionsRef[1].valeurs.map(valeur =>
+                    sql`${oeuvres_musee.artiste} = ${valeur.toLowerCase()}`
+                );
+
+                const conditionsRefMouvement = selectionsRef[2].valeurs.map(valeur =>
+                    sql`${oeuvres_musee.mouvement} = ${valeur.toLowerCase()}`
+                );
+
+                const queryAcceptConditionTypeOeuvre = sql.join(conditionsAcceptTypeOeuvre, sql` OR `);
+                const queryAcceptConditionArtiste = sql.join(conditionsAcceptArtiste, sql` OR `);
+                const queryAcceptConditionMouvement = sql.join(conditionsAcceptMouvement, sql` OR `);
+
+                const queryRefConditionTypeOeuvre = sql.join(conditionsRefTypeOeuvre, sql` OR `);
+                const queryRefConditionArtiste = sql.join(conditionsRefArtiste, sql` OR `);
+                const queryRefConditionMouvement = sql.join(conditionsRefMouvement, sql` OR `);
+
+                const TEMP_Accepttab = [queryAcceptConditionTypeOeuvre, queryAcceptConditionArtiste, queryAcceptConditionMouvement];
+                const TEMP_Reftab = [queryRefConditionTypeOeuvre, queryRefConditionArtiste, queryRefConditionMouvement];
+
+                const queryAcceptCondition = sql.join(TEMP_Accepttab, sql` OR `);
+                const queryRefCondition = sql.join(TEMP_Reftab, sql` AND `);
+
+                const TempQueryCondition = [queryAcceptCondition, queryRefCondition];
+
+                const queryCondition = sql.join(TempQueryCondition, sql` OR `)
+
+                const dbResult = await db
+                    .select()
+                    .from(oeuvres_musee)
+                    .where(queryCondition);
+
+                const formattedResult = dbResult.reduce((acc, item) => {
+                    acc[item.id] = {
+                        nom: item.nom,
+                        description: item.description,
+                        type_oeuvre: item.type_oeuvre,
+                        artiste: item.artiste,
+                        mouvement: item.mouvement,
+                        x: item.x,
+                        y: item.y
+                    };
+                    return acc;
+                }, {});
+                return NextResponse.json(formattedResult);
+            }
             else{
                 const dbResult = await db.select().from(oeuvres_musee);
                 return NextResponse.json(dbResult);
