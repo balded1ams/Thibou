d878d661467811f0ff06e258935623d35e883ee8
Paul <paaul.rbn@gmail.com>
2025-01-21 14:10:32 +0100

    fix les trucs

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,7 +2,12 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
 const pool = new Pool({
-    connectionString : process.env.PSQL_DATABASE
+    host: process.env.PSQL_HOST ,
+    port: parseInt(process.env.PSQL_PORT || "5432"),
+    user: process.env.PSQL_USER ,
+    password: process.env.PSQL_PASSWORD,
+    database: process.env.PSQL_DB,
+    connectionString : process.env.PSQL_DATABASE,
 });
 
 const db = drizzle(pool);
