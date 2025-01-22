3c3ec3e24f8c0296ee928aea5bb18aa34bcabcbe
mbelondm <Manohisoa.Mbelo-Ndriamanampy@etu.univ-grenoble-alpes.fr>
2025-01-22 20:18:58 +0100

    Feat: Ajout de modifier compte, supprimer compte et page not found

--- src/app/404.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div style={{ textAlign: 'center', padding: '50px' }}>
-      <h1>404 - Page Not Found</h1>
-      <p>La page que vous cherchez n'existe pas.</p>
-    </div>
-  );
-}
--- src/app/editUser/page.tsx
+++ src/app/editUser/page.tsx
@@ -1,15 +1,16 @@
+
 import React from "react";
-import EditUserComponent from "../../components/editUser";
+import EditUserComponent from "./editUser";
 import { headers } from "next/headers";
 import { getIdUserFromSession } from "../../../script/session";
 import { fetchUtilisateur } from "../../../script/slugify";
 import { utilisateurType } from "@/types";
 
 const EditUser = async () => {
-    const headersList = headers();
-    const idUser = await getIdUserFromSession(await headersList);
+    const headersList = await headers();
+    const idUser = await getIdUserFromSession(headersList);
     let userConnected: utilisateurType | null = null;
-    
+
     if (idUser) {
         userConnected = await fetchUtilisateur(idUser);
     }
