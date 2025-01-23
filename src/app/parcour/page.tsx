fa26f25717e08f99a389b9cc8841494eeba3a39a
Chlorate <bald3diams@gmail.com>
2025-01-23 04:47:58 +0100

    fetch des sauvegardes fonctionnel

--- src/app/parcour/page.tsx
+++ src/app/parcour/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 
@@ -7,24 +7,21 @@ import Plan from "@/components/plan";
 import Guide from "@/components/guide";
 
 import { useThemeContext } from "@/hooks/useTheme";
-import {pathing} from "@/hooks/useBFS";
+import { pathing } from "@/hooks/useBFS";
 
 export default function FracPage() {
-
-  // eslint-disable-next-line react-hooks/rules-of-hooks
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const advancePoint = () => {
-    setCurrentIndex(prevIndex => prevIndex + 1);
-    //addOutput(`oeuvre: ${currentIndex}`)
+    setCurrentIndex((prevIndex) => prevIndex + 1);
     savePoint();
-    fetchHistorique()
+    fetchSauvegarde();
   };
 
-  const fetchHistorique = async () => {
+  const fetchSauvegarde = async () => {
     try {
-      const response = await fetch('/api/fetchSauvegarde', {
-        method: 'GET',
+      const response = await fetch("/api/fetchSauvegarde", {
+        method: "GET",
       });
 
       if (!response.ok) {
@@ -33,17 +30,19 @@ export default function FracPage() {
 
       const data = await response.json();
       console.log(data);
+      return data;
     } catch (error) {
-      console.error('Erreur lors de la récupération des données :', error);
+      console.error("Erreur lors de la récupération des données :", error);
+      return null;
     }
   };
 
   const savePoint = async () => {
     try {
-      const response = await fetch('/api/sauvegarde', {
-        method: 'POST',
+      const response = await fetch("/api/sauvegarde", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           trajet_restant: result,
@@ -57,21 +56,36 @@ export default function FracPage() {
       const data = await response.json();
       console.log(data);
     } catch (error) {
-      console.error('Erreur lors de la récupération des données :', error);
+      console.error("Erreur lors de l'enregistrement des données :", error);
     }
   };
 
   const [result, setResult] = useState<[number, number][][]>([]);
   const [dataLoaded, setDataLoaded] = useState(false);
 
+  const isConnected = () => {
+    return true;
+  };
+
   useEffect(() => {
-    const fetchPoints = async () => {
-      const result = await pathing();
-      setResult(result);
+    const fetchData = async () => {
+      if (isConnected()) {
+        const sauvegarde = await fetchSauvegarde();
+        if (sauvegarde && sauvegarde.trajet_restant?.length) {
+          setResult(sauvegarde.trajet_restant);
+        } else {
+          const result = await pathing();
+          setResult(result);
+        }
+      } else {
+        const result = await pathing();
+        setResult(result);
+      }
       setDataLoaded(true);
     };
+
     if (!dataLoaded) {
-      fetchPoints();
+      fetchData();
     }
   }, [dataLoaded]);
 
@@ -79,23 +93,21 @@ export default function FracPage() {
   const { systemTheme } = useThemeContext();
 
   return (
-    <div
-      className="min-h-screen w-full overflow-y-auto"
-      style={{
-        backgroundColor: systemTheme.background.primary,
-        color: systemTheme.text.primary,
-      }}
-    >
-      <main className="mx-auto flex h-full min-h-screen max-w-5xl flex-col gap-4 px-4 xl:px-0 mb-8">
-        <Header />
-        <div className="flex flex-col gap-4 xl:flex-row">
-          <Plan currentIndex={currentIndex}  path={result}/>
-          <Guide
-            onSuivant={advancePoint}
-          />
-        </div>
-      </main>
-      <Footer />
-    </div>
+      <div
+          className="min-h-screen w-full overflow-y-auto"
+          style={{
+            backgroundColor: systemTheme.background.primary,
+            color: systemTheme.text.primary,
+          }}
+      >
+        <main className="mx-auto flex h-full min-h-screen max-w-5xl flex-col gap-4 px-4 xl:px-0 mb-8">
+          <Header />
+          <div className="flex flex-col gap-4 xl:flex-row">
+            <Plan currentIndex={currentIndex} path={result} />
+            <Guide onSuivant={advancePoint} />
+          </div>
+        </main>
+        <Footer />
+      </div>
   );
 }
