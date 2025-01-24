578cad0288c05c5109fa94ae50d84e783c4ac8d9
arowana755 <silure.38@gmail.com>
2025-01-24 08:28:16 +0100

    Useless headers no longer imported

--- src/app/afficheUser/page.tsx
+++ src/app/afficheUser/page.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ViewUserComponent from "@/components/viewUser";
-import { headers } from "next/headers";
 import { getIdUserFromSession } from "../../../script/session";
 import { fetchUtilisateur } from "../../../script/slugify";
 import { utilisateurType } from "@/types";
 
 const EditUser = async () => {
-    const headersList = headers();
     const idUser = await getIdUserFromSession(await headersList);
     let userConnected: utilisateurType | null = null;
     
